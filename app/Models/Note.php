0b6d98922ff2b7fd66e602a610ebc40a873dfb8e
anilaeusphp <anilaeus@gmail.com>
2024-01-07 00:53:00 +0300

    52:40

---
+++ app/Models/Note.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Note extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'title',
+        'body',
+        'send_date',
+        'is_published',
+        'heart_count',
+    ];
+
+    public function user():BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function publishedNotes(User $user) : array
+    {
+        return $this->where('user_id', $user->id)->where('is_published', true)->get();
+    }
+}
